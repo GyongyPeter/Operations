ae73d3f4d7101c387e6a729a95ee8d20fc023480
Morvai Daniel <up201911214@FEUPSIG.FE.UP.PT>
2020-02-17 18:26:08 +0000

    new operation line

--- src/Application.java
+++ src/Application.java
@@ -4,9 +4,19 @@ public class Application {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        int a = scanner.nextInt();
-        int b = scanner.nextInt();
+        String operation = scanner.nextLine();
 
-        System.out.println(a + b);
+        if (operation.equals("sum")) {
+            int a = scanner.nextInt();
+            int b = scanner.nextInt();
+
+            System.out.println(a + b);
+
+        } else if (operation.equals("mul")) {
+            int a = scanner.nextInt();
+            int b = scanner.nextInt();
+
+            System.out.println(a * b);
+        }
     }
 }
